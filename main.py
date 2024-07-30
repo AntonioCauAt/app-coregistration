b4ee4a393f9bd71afa5b7166ad047ce03f58210e
AntonioCauAt <antoniocaulinatienzar@gmail.com>
2024-07-30 11:07:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
+# set up environment, all are necessary for coregistration
 import os
 import json
 import mne
-
-import matplotlib.pyplot as plt
+from mne.coreg import Coregistration
+from mne.io import read_info
 import numpy as np
-
 
 # Current path
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-# Load inputs from config.json
-with open('config.json') as config_json:
+# Populate mne_config.py file with brainlife config.json
+with open(__location__+'/config.json') as config_json:
     config = json.load(config_json)
-    
-# == LOAD DATA (mne/raw) ==
-fname = config['mne']
-raw = mne.io.read_raw_fif(fname, preload=True)
 
-# == LOAD DATA (bem) ==
+#Read the file in
+fname_epo = config['epo']
 
-# subjects_dir: path to the directory containing the FreeSurfer subjects reconstructions (SUBJECTS_DIR)
-subjects_dir = config['output']
-subject= 'output'
-mne.gui.coregistration(subject=subject, subjects_dir=subjects_dir, inst=raw)
+#Read the subject
+subject = config['subject']
 
-# Start MNE-Report
-report = mne.Report(title='Report')
+#Read the subject dir
+subj_dir = config['subj_dir']
 
-#add report coregistration 
+#read the information about the file of events
+info = read_info(fname_epo)
 
-# == SAVE REPORT ==
-report.save(os.path.join('out_dir_report','report.html'))
+#configure some parameters of visualization
+plot_kwargs = dict(
+    subject=subject,
+    subjects_dir=subj_dir,
+    surfaces="head-dense",
+    dig=True,
+    eeg=[],
+    meg="sensors",
+    show_axes=True,
+    coord_frame="meg",
+)
+view_kwargs = dict(azimuth=45, elevation=90, distance=0.6, focalpoint=(0.0, 0.0, 0.0))
 
-# == SAVE DATA ==
-raw.save(os.path.join('out_dir','raw.fif'))
+fiducials = "estimated"  # get fiducials from fsaverage
+coreg = Coregistration(info, subject, subj_dir, fiducials=fiducials)
+
+coreg.fit_fiducials(verbose=True)
+
+coreg.fit_icp(n_iterations=6, nasion_weight=2.0, verbose=True)
+coreg.omit_head_shape_points(distance=5.0 / 1000)  # distance is in meters
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
+
+if config['final'] == True:
+    coreg.fit_icp(n_iterations=20, nasion_weight=10.0, verbose=True)
+    fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs)
+    mne.viz.set_3d_view(fig, **view_kwargs)
+
+    dists = coreg.compute_dig_mri_distances() * 1e3  # in mm
+    print(
+        f"Distance between HSP and MRI (mean/min/max):\n{np.mean(dists):.2f} mm "
+        f"/ {np.min(dists):.2f} mm / {np.max(dists):.2f} mm"
+    )
+
